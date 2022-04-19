5118058c1dbac57abef959f73881e6ce3c58d32e
HeyangXue1997 <1139470232@qq.com>
2022-04-19 11:29:21 +0800

    Add files via upload

--- code/text/__init__.py
+++ code/text/__init__.py
@@ -1,1 +1,7 @@
+from text.symbols import *
 
+ttsing_pitch_to_id = {p: i for i, p, in enumerate(ttsing_pitch_set)}
+id_to_ttsing_pitch = {i: p for i, p, in enumerate(ttsing_pitch_set)}
+
+learn2sing_pho_to_id = {p: i for i, p, in enumerate(learn2sing_phone_set)}
+learn2sing_id_to_pho = {p: i for i, p, in enumerate(learn2sing_phone_set)}
---
+++ code/text/symbols.py
@@ -0,0 +1,3 @@
+#implment your own phone set and pitch set
+ttsing_pitch_set = ['A3', 'B3']
+learn2sing_phone_set = ['t', 's', 'e', 'sh', 'iii', 'iou', 'd', 'ian', 'l', 'an']
